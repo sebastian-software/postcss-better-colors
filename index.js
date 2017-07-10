84ef3088381f0163845d07a1c5e2feca7465fe64
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-10 17:05:42 +0200

    Auto formatting using prettier

--- index.js
+++ index.js
@@ -1,89 +1,83 @@
 import postcss from "postcss"
-import helpers from 'postcss-message-helpers'
-import webcolors from 'webcolors'
+import helpers from "postcss-message-helpers"
+import webcolors from "webcolors"
 
-var DEFAULTS = webcolors.mrmrs;
+var DEFAULTS = webcolors.mrmrs
 
 // All props that use the <color> data type
 // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#See_also
 var PROPS = [
-  'color',
-  'background',
-  'background-color',
-  'border',
-  'border-color',
-  'outline',
-  'outline-color',
-  'text-shadow',
-  'box-shadow'
-];
+  "color",
+  "background",
+  "background-color",
+  "border",
+  "border-color",
+  "outline",
+  "outline-color",
+  "text-shadow",
+  "box-shadow"
+]
 
 // CSS color keywords to replace
 var KEYWORDS = [
-  'aqua',
-  'black',
-  'blue',
-  'fuchsia',
-  'gray',
-  'green',
-  'lime',
-  'maroon',
-  'navy',
-  'olive',
-  'orange',
-  'purple',
-  'red',
-  'silver',
-  'teal',
-  'white',
-  'yellow'
-];
+  "aqua",
+  "black",
+  "blue",
+  "fuchsia",
+  "gray",
+  "green",
+  "lime",
+  "maroon",
+  "navy",
+  "olive",
+  "orange",
+  "purple",
+  "red",
+  "silver",
+  "teal",
+  "white",
+  "yellow"
+]
 
-var KEYWORD_REGEX = new RegExp('\\b(' + KEYWORDS.join('|') + ')\\b');
+var KEYWORD_REGEX = new RegExp("\\b(" + KEYWORDS.join("|") + ")\\b")
 
+export default postcss.plugin("postcss-color-palette", function(opts) {
+  opts = opts || {}
+  opts.palette = opts.palette || DEFAULTS
 
-export default postcss.plugin("postcss-color-palette", function (opts) {
-  opts = opts || {};
-  opts.palette = opts.palette || DEFAULTS;
-
-  if (typeof opts.palette === 'string') {
+  if (typeof opts.palette === "string") {
     if (webcolors.hasOwnProperty(opts.palette)) {
-      opts.palette = webcolors[opts.palette];
+      opts.palette = webcolors[opts.palette]
     } else {
-      throw new Error('Unknown webcolors palette: "' + opts.palette + '"');
+      throw new Error('Unknown webcolors palette: "' + opts.palette + '"')
     }
   }
 
-  var palette    = opts.palette;
-  var transforms = [];
+  var palette = opts.palette
+  var transforms = []
 
   // For each color keyword, generate a [RegExp, 'replacement'] pair,
   // i.e. the arguments to String.prototype.replace
-  KEYWORDS.forEach(function (keyword) {
+  KEYWORDS.forEach(function(keyword) {
     if (palette.hasOwnProperty(keyword) && palette[keyword]) {
-      transforms.push([
-        new RegExp('\\b(' + keyword + ')(\\s*([^(]|$))', 'gi'),
-        palette[keyword] + '$2'
-      ]);
+      transforms.push([new RegExp("\\b(" + keyword + ")(\\s*([^(]|$))", "gi"), palette[keyword] + "$2"])
     }
-  });
+  })
 
-  return function processor (css) {
-    css.walkDecls(function transformDecl (decl) {
-      console.log(decl)
+  return function processor(css) {
+    css.walkDecls(function transformDecl(decl) {
       // Check if the decl is of a color-related property and make sure
       // it has a value containing a replaceable color
-      if (PROPS.indexOf(decl.prop) === -1 ||
-          !decl.value ||
-          !KEYWORD_REGEX.test(decl.value)) {
-        return;
+      if (PROPS.indexOf(decl.prop) === -1 || !decl.value || !KEYWORD_REGEX.test(decl.value)) {
+        return
       }
+
       // Transform!
-      decl.value = helpers.try(function transformValue () {
-        return transforms.reduce(function (value, args) {
-          return value.replace.apply(value, args);
-        }, decl.value);
-      }, decl.source);
-    });
-  };
-});
+      decl.value = helpers.try(function transformValue() {
+        return transforms.reduce(function(value, args) {
+          return value.replace.apply(value, args)
+        }, decl.value)
+      }, decl.source)
+    })
+  }
+})
