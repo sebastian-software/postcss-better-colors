bef8ccd943100fe336e3d8c51f7237281a5c3a88
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-10 17:19:40 +0200

    Added more tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,32 @@
 import postcss from "postcss"
 import plugin from "."
 
-test("Transform Colors", () => {
+test("Transform Red", () => {
   return postcss([ plugin ]).process(".red { color: red; }").then((result) => {
+    expect(result.css).toMatchSnapshot()
+  })
+})
+
+test("Transform Blue", () => {
+  return postcss([ plugin ]).process(".blue { color: blue; }").then((result) => {
+    expect(result.css).toMatchSnapshot()
+  })
+})
+
+test("Transform Green", () => {
+  return postcss([ plugin ]).process(".green { color: green; }").then((result) => {
+    expect(result.css).toMatchSnapshot()
+  })
+})
+
+test("Transform Orange", () => {
+  return postcss([ plugin ]).process(".orange { color: orange; }").then((result) => {
+    expect(result.css).toMatchSnapshot()
+  })
+})
+
+test("Transform Yellow", () => {
+  return postcss([ plugin ]).process(".yellow { color: yellow; }").then((result) => {
     expect(result.css).toMatchSnapshot()
   })
 })
